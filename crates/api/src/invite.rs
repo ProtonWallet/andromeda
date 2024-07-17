2c5f9cbbf1915d99a23adc9a870896ba35c1f2e4
Kévin Ta <kevin.ta@proton.ch>
2024-07-17 10:53:11 +0000

    Update invitation route

--- crates/api/src/invite.rs
+++ crates/api/src/invite.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
 
 use crate::{
     core::{ApiClient, ProtonResponseExt},
@@ -8,10 +9,28 @@ use crate::{
     ProtonWalletApiClient, BASE_WALLET_API_V1,
 };
 
+#[derive(Deserialize_repr, Serialize_repr, PartialEq, Debug)]
+#[repr(u8)]
+pub enum InviteNotificationType {
+    Newcomer = 1,
+    EmailIntegration = 2,
+    #[serde(other)]
+    Unsupported,
+}
+
 #[derive(Debug, Serialize)]
 #[allow(non_snake_case)]
 pub struct InviteRequestBody {
     pub Email: String,
+    pub Type: InviteNotificationType,
+    pub InviterAddressID: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(non_snake_case)]
+pub struct CanSendInviteResponseBody {
+    pub Code: u16,
+    pub CanSend: u8,
 }
 
 #[derive(Debug, Deserialize)]
@@ -40,10 +59,12 @@ impl ApiClient for InviteClient {
 }
 
 impl InviteClient {
-    pub async fn send_newcomer_invite(&self, invitee_email: String) -> Result<(), Error> {
-        let request = self
-            .post("invites")
-            .body_json(InviteRequestBody { Email: invitee_email })?;
+    pub async fn send_newcomer_invite(&self, invitee_email: String, inviter_address_id: String) -> Result<(), Error> {
+        let request = self.post("invites").body_json(InviteRequestBody {
+            Email: invitee_email,
+            Type: InviteNotificationType::Newcomer,
+            InviterAddressID: inviter_address_id,
+        })?;
 
         let response = self.api_client.send(request).await?;
         response.parse_response::<SendInviteResponseBody>()?;
@@ -53,17 +74,34 @@ impl InviteClient {
 
     /// Call an endpoint to check whether or not user can send an invite to the
     /// requested email. Throw if user cannot send, resolves if user can send
-    pub async fn check_invite_status(&self, invitee_email: String) -> Result<(), Error> {
-        let request = self.get("invites").query(("Email", invitee_email));
-        self.api_client.send(request).await?;
+    pub async fn check_invite_status(
+        &self,
+        invitee_email: String,
+        invite_notification_type: InviteNotificationType,
+        inviter_address_id: String,
+    ) -> Result<u8, Error> {
+        let request = self
+            .get("invites")
+            .query(("Email", invitee_email))
+            .query(("Type", (invite_notification_type as i32).to_string()))
+            .query(("InviterAddressID", inviter_address_id));
 
-        Ok(())
+        let response = self.api_client.send(request).await?;
+        let parsed = response.parse_response::<CanSendInviteResponseBody>()?;
+
+        Ok(parsed.CanSend)
     }
 
-    pub async fn send_email_integration_invite(&self, invitee_email: String) -> Result<(), Error> {
-        let request = self
-            .post("invites/email-integration")
-            .body_json(InviteRequestBody { Email: invitee_email })?;
+    pub async fn send_email_integration_invite(
+        &self,
+        invitee_email: String,
+        inviter_address_id: String,
+    ) -> Result<(), Error> {
+        let request = self.post("invites").body_json(InviteRequestBody {
+            Email: invitee_email,
+            Type: InviteNotificationType::EmailIntegration,
+            InviterAddressID: inviter_address_id,
+        })?;
         let response = self.api_client.send(request).await?;
         response.parse_response::<SendInviteResponseBody>()?;
 
@@ -80,7 +118,10 @@ mod tests {
         Mock, MockServer, ResponseTemplate,
     };
 
-    use crate::{core::ApiClient, tests::utils::setup_test_connection, InviteClient, BASE_WALLET_API_V1};
+    use crate::{
+        core::ApiClient, invite::InviteNotificationType, tests::utils::setup_test_connection, InviteClient,
+        BASE_WALLET_API_V1,
+    };
 
     #[tokio::test]
     async fn test_send_newcomer_invite_success() {
@@ -92,6 +133,8 @@ mod tests {
         );
         let req_path: String = format!("{}/invites", BASE_WALLET_API_V1);
         let response = ResponseTemplate::new(200).set_body_json(response_body);
+        let inviter_address_id =
+            "lY2ZCYkVNfl_osze70PRoqzg34MQI64mE3-pLc-yMp_6KXthkV1paUsyS276OdNwucz9zKoWKZL_TgtKxOPb0w==";
         Mock::given(method("POST"))
             .and(path(req_path))
             .respond_with(response)
@@ -99,7 +142,9 @@ mod tests {
             .await;
         let api_client = setup_test_connection(mock_server.uri());
         let client = InviteClient::new(Arc::new(api_client));
-        let result = client.send_newcomer_invite("test@pm.me".to_owned()).await;
+        let result = client
+            .send_newcomer_invite("test@pm.me".to_owned(), inviter_address_id.to_owned())
+            .await;
         match result {
             Ok(_) => return,
             Err(_) => panic!("Expected Ok variant but got Err."),
@@ -107,24 +152,35 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_get_invite_status_success() {
+    async fn test_get_invite_status_newcomer_success() {
         let mock_server = MockServer::start().await;
         let response_body = serde_json::json!(
             {
-                "Code": 1000
+                "Code": 1000,
+                "CanSend": 1
             }
         );
         let req_path: String = format!("{}/invites", BASE_WALLET_API_V1);
         let response: ResponseTemplate = ResponseTemplate::new(200).set_body_json(response_body);
+        let inviter_address_id =
+            "lY2ZCYkVNfl_osze70PRoqzg34MQI64mE3-pLc-yMp_6KXthkV1paUsyS276OdNwucz9zKoWKZL_TgtKxOPb0w==";
         Mock::given(method("GET"))
             .and(path(req_path))
             .and(query_param("Email", "test@pm.me"))
+            .and(query_param("Type", "1"))
+            .and(query_param("InviterAddressID", inviter_address_id))
             .respond_with(response)
             .mount(&mock_server)
             .await;
         let api_client = setup_test_connection(mock_server.uri());
         let client = InviteClient::new(Arc::new(api_client));
-        let result = client.check_invite_status("test@pm.me".to_owned()).await;
+        let result = client
+            .check_invite_status(
+                "test@pm.me".to_owned(),
+                InviteNotificationType::Newcomer,
+                inviter_address_id.to_owned(),
+            )
+            .await;
         match result {
             Ok(_) => return,
             Err(_) => panic!("Expected Ok variant but got Err."),
@@ -132,15 +188,52 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_send_email_integration_invitaion_success() {
+    async fn test_get_invite_status_email_integration_success() {
         let mock_server = MockServer::start().await;
         let response_body = serde_json::json!(
             {
-                "Code": 1000
+                "Code": 1000,
+                "CanSend": 1
             }
         );
-        let req_path: String = format!("{}/invites/email-integration", BASE_WALLET_API_V1);
+        let req_path: String = format!("{}/invites", BASE_WALLET_API_V1);
+        let response: ResponseTemplate = ResponseTemplate::new(200).set_body_json(response_body);
+        let inviter_address_id =
+            "lY2ZCYkVNfl_osze70PRoqzg34MQI64mE3-pLc-yMp_6KXthkV1paUsyS276OdNwucz9zKoWKZL_TgtKxOPb0w==";
+        Mock::given(method("GET"))
+            .and(path(req_path))
+            .and(query_param("Email", "test@pm.me"))
+            .and(query_param("Type", "2"))
+            .respond_with(response)
+            .mount(&mock_server)
+            .await;
+        let api_client = setup_test_connection(mock_server.uri());
+        let client = InviteClient::new(Arc::new(api_client));
+        let result = client
+            .check_invite_status(
+                "test@pm.me".to_owned(),
+                InviteNotificationType::EmailIntegration,
+                inviter_address_id.to_owned(),
+            )
+            .await;
+        match result {
+            Ok(_) => return,
+            Err(_) => panic!("Expected Ok variant but got Err."),
+        }
+    }
+
+    #[tokio::test]
+    async fn test_send_email_integration_invitation_success() {
+        let mock_server = MockServer::start().await;
+        let response_body = serde_json::json!(
+            {
+                "Code": 1000,
+            }
+        );
+        let req_path: String = format!("{}/invites", BASE_WALLET_API_V1);
         let response = ResponseTemplate::new(200).set_body_json(response_body);
+        let inviter_address_id =
+            "lY2ZCYkVNfl_osze70PRoqzg34MQI64mE3-pLc-yMp_6KXthkV1paUsyS276OdNwucz9zKoWKZL_TgtKxOPb0w==";
         Mock::given(method("POST"))
             .and(path(req_path))
             .respond_with(response)
@@ -148,7 +241,9 @@ mod tests {
             .await;
         let api_client = setup_test_connection(mock_server.uri());
         let client = InviteClient::new(Arc::new(api_client));
-        let result = client.send_email_integration_invite("test@pm.me".to_owned()).await;
+        let result = client
+            .send_email_integration_invite("test@pm.me".to_owned(), inviter_address_id.to_owned())
+            .await;
         match result {
             Ok(_) => return,
             Err(_) => panic!("Expected Ok variant but got Err."),
--- crates/wasm/src/api/invite.rs
+++ crates/wasm/src/api/invite.rs
@@ -1,7 +1,37 @@
-use andromeda_api::invite::InviteClient;
+use andromeda_api::invite::{InviteClient, InviteNotificationType};
+use serde::{Deserialize, Serialize};
+use tsify::Tsify;
 use wasm_bindgen::prelude::*;
 
 use crate::common::error::ErrorExt;
+
+#[derive(Tsify, Serialize, Deserialize, Clone)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub enum WasmInviteNotificationType {
+    Newcomer,
+    EmailIntegration,
+    Unsupported,
+}
+
+impl From<InviteNotificationType> for WasmInviteNotificationType {
+    fn from(value: InviteNotificationType) -> Self {
+        match value {
+            InviteNotificationType::Newcomer => WasmInviteNotificationType::Newcomer,
+            InviteNotificationType::EmailIntegration => WasmInviteNotificationType::EmailIntegration,
+            InviteNotificationType::Unsupported => WasmInviteNotificationType::Unsupported,
+        }
+    }
+}
+
+impl From<WasmInviteNotificationType> for InviteNotificationType {
+    fn from(value: WasmInviteNotificationType) -> Self {
+        match value {
+            WasmInviteNotificationType::Newcomer => InviteNotificationType::Newcomer,
+            WasmInviteNotificationType::EmailIntegration => InviteNotificationType::EmailIntegration,
+            WasmInviteNotificationType::Unsupported => InviteNotificationType::Unsupported,
+        }
+    }
+}
 
 #[wasm_bindgen]
 #[derive(Clone)]
@@ -16,25 +46,34 @@ impl From<InviteClient> for WasmInviteClient {
 #[wasm_bindgen]
 impl WasmInviteClient {
     #[wasm_bindgen(js_name = "sendNewcomerInvite")]
-    pub async fn send_newcomer_invite(&self, invitee_email: String) -> Result<(), JsValue> {
+    pub async fn send_newcomer_invite(&self, invitee_email: String, inviter_address_id: String) -> Result<(), JsValue> {
         self.0
-            .send_newcomer_invite(invitee_email)
+            .send_newcomer_invite(invitee_email, inviter_address_id)
             .await
             .map_err(|e| e.to_js_error())
     }
 
     #[wasm_bindgen(js_name = "checkInviteStatus")]
-    pub async fn check_invite_status(&self, invitee_email: String) -> Result<(), JsValue> {
+    pub async fn check_invite_status(
+        &self,
+        invitee_email: String,
+        invite_notification_type: WasmInviteNotificationType,
+        inviter_address_id: String,
+    ) -> Result<u8, JsValue> {
         self.0
-            .check_invite_status(invitee_email)
+            .check_invite_status(invitee_email, invite_notification_type.into(), inviter_address_id)
             .await
             .map_err(|e| e.to_js_error())
     }
 
     #[wasm_bindgen(js_name = "sendEmailIntegrationInvite")]
-    pub async fn send_email_integration_invite(&self, invitee_email: String) -> Result<(), JsValue> {
+    pub async fn send_email_integration_invite(
+        &self,
+        invitee_email: String,
+        inviter_address_id: String,
+    ) -> Result<(), JsValue> {
         self.0
-            .send_email_integration_invite(invitee_email)
+            .send_email_integration_invite(invitee_email, inviter_address_id)
             .await
             .map_err(|e| e.to_js_error())
     }
