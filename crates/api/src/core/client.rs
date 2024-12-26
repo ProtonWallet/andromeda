886b1c98cdbd76ba90a7f8000a2cd610c58e2911
Will Hsu <will.hsu@proton.ch>
2024-12-26 10:34:34 +0000

    Try read default time constraint from env so we can customize time constraint for atlas

--- crates/api/src/core/client.rs
+++ crates/api/src/core/client.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use muon::ProtonRequest;
 
 use super::ToProtonRequest;
-use crate::{ProtonWalletApiClient, DEFAULT_INTERACTIVITY, DEFAULT_SERVICE_TYPE, DEFAULT_TIME_CONSTRAINT};
+use crate::{get_default_time_constraint, ProtonWalletApiClient, DEFAULT_INTERACTIVITY, DEFAULT_SERVICE_TYPE};
 
 pub trait ApiClient {
     fn new(api_client: Arc<ProtonWalletApiClient>) -> Self;
@@ -15,28 +15,28 @@ pub trait ApiClient {
         return self
             .build_request(self.base_url(), endpoint)
             .to_get_request()
-            .allowed_time(DEFAULT_TIME_CONSTRAINT)
+            .allowed_time(get_default_time_constraint())
             .service_type(DEFAULT_SERVICE_TYPE, true);
     }
     fn post(&self, endpoint: impl ToString) -> ProtonRequest {
         return self
             .build_request(self.base_url(), endpoint)
             .to_post_request()
-            .allowed_time(DEFAULT_TIME_CONSTRAINT)
+            .allowed_time(get_default_time_constraint())
             .service_type(DEFAULT_INTERACTIVITY, true);
     }
     fn put(&self, endpoint: impl ToString) -> ProtonRequest {
         return self
             .build_request(self.base_url(), endpoint)
             .to_put_request()
-            .allowed_time(DEFAULT_TIME_CONSTRAINT)
+            .allowed_time(get_default_time_constraint())
             .service_type(DEFAULT_INTERACTIVITY, true);
     }
     fn delete(&self, endpoint: impl ToString) -> ProtonRequest {
         return self
             .build_request(self.base_url(), endpoint)
             .to_delete_request()
-            .allowed_time(DEFAULT_TIME_CONSTRAINT)
+            .allowed_time(get_default_time_constraint())
             .service_type(DEFAULT_INTERACTIVITY, true);
     }
     fn build_request(&self, version: &str, endpoint: impl ToString) -> String {
