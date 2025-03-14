3e711bde78bddfdf22c68fda3d5032acda88b1b9
Steven Lin <yu-cheng.lin@proton.ch>
2025-03-14 06:00:09 +0000

    Add Watch-only wallet support

--- crates/bitcoin/src/wallet.rs
+++ crates/bitcoin/src/wallet.rs
@@ -1,5 +1,5 @@
 use core::fmt::Debug;
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, str::FromStr, sync::Arc};
 
 use andromeda_api::ProtonWalletApiClient;
 use andromeda_common::{FromParts, Network, ScriptType};
@@ -11,6 +11,7 @@ use bdk_wallet::{
     },
     Balance,
 };
+use bitcoin::bip32::Xpub;
 use futures::future::try_join_all;
 
 use super::{account::Account, transactions::Pagination, utils::sort_and_paginate_txs};
@@ -28,9 +29,22 @@ use crate::{
 const ACCOUNT_DISCOVERY_STOP_GAP: u32 = 2;
 const ADDRESS_DISCOVERY_STOP_GAP: usize = 10;
 
+#[derive(Debug, PartialEq)]
+pub enum WalletType {
+    OnChain,
+    Lightning,
+    WatchOnly,
+}
+
+#[derive(Debug)]
+enum WalletMasterKey {
+    Xpriv(Xpriv),
+    Xpub(Xpub),
+}
+
 #[derive(Debug)]
 pub struct Wallet {
-    mprv: Xpriv,
+    key: WalletMasterKey,
     accounts: HashMap<DerivationPath, Arc<Account>>,
     network: Network,
 }
@@ -54,14 +68,36 @@ impl Wallet {
         .unwrap();
 
         Ok(Wallet {
-            mprv,
+            key: WalletMasterKey::Xpriv(mprv),
             accounts: HashMap::new(),
             network,
         })
     }
 
-    pub fn mprv(&self) -> (Xpriv, Network) {
-        (self.mprv, self.network)
+    pub fn new_watch_only(network: Network, xpub: String) -> Result<Self, Error> {
+        let xpub = Xpub::from_str(&xpub)?;
+
+        Ok(Wallet {
+            key: WalletMasterKey::Xpub(xpub),
+            accounts: HashMap::new(),
+            network,
+        })
+    }
+
+    pub fn mprv(&self) -> (Option<Xpriv>, Network) {
+        if let WalletMasterKey::Xpriv(mprv) = &self.key {
+            return (Some(mprv.clone()), self.network);
+        } else {
+            return (None, self.network);
+        }
+    }
+
+    pub fn xpub(&self) -> (Option<Xpub>, Network) {
+        if let WalletMasterKey::Xpub(xpub) = &self.key {
+            return (Some(xpub.clone()), self.network);
+        } else {
+            return (None, self.network);
+        }
     }
 
     pub fn add_account<F>(
@@ -74,16 +110,29 @@ impl Wallet {
         F: WalletPersisterFactory,
     {
         let secp = Secp256k1::new();
-        let store_key = format!("{}_{}", self.mprv.fingerprint(&secp), derivation_path);
-        let persister = factory.build(store_key);
-
-        let account = Account::new(
-            self.mprv,
-            self.network,
-            script_type,
-            derivation_path,
-            WalletStorage(persister),
-        )?;
+        let account = if let WalletMasterKey::Xpriv(mprv) = self.key {
+            let store_key = format!("{}_{}", mprv.fingerprint(&secp), derivation_path.to_string());
+            let persister = factory.build(store_key);
+            Account::new(
+                mprv,
+                self.network,
+                script_type,
+                derivation_path,
+                WalletStorage(persister),
+            )?
+        } else if let WalletMasterKey::Xpub(xpub) = self.key {
+            let store_key = format!("{}_{}", xpub.fingerprint(), derivation_path.to_string());
+            let persister = factory.build(store_key);
+            Account::new_with_xpub(
+                xpub,
+                script_type,
+                self.network,
+                derivation_path,
+                WalletStorage(persister),
+            )?
+        } else {
+            return Err(Error::WalletNotInitialized);
+        };
 
         let derivation_path = account.get_derivation_path();
 
@@ -152,17 +201,32 @@ impl Wallet {
                 let derivation_path = DerivationPath::from_parts(script_type, self.network, index);
 
                 let secp = Secp256k1::new();
-                let store_key = format!("{}_{}", self.mprv.fingerprint(&secp), derivation_path);
-                let persister = factory.clone().build(store_key);
 
-                let account = Account::new(
-                    self.mprv,
-                    self.network,
-                    script_type,
-                    derivation_path.clone(),
-                    WalletStorage(persister),
-                )
-                .expect("Account should be valid here");
+                let account = if let WalletMasterKey::Xpriv(mprv) = self.key {
+                    let store_key = format!("{}_{}", mprv.fingerprint(&secp), derivation_path);
+                    let persister = factory.clone().build(store_key);
+                    Account::new(
+                        mprv,
+                        self.network,
+                        script_type,
+                        derivation_path.clone(),
+                        WalletStorage(persister),
+                    )
+                    .expect("Account should be valid here")
+                } else if let WalletMasterKey::Xpub(xpub) = self.key {
+                    let store_key = format!("{}_{}", xpub.fingerprint(), derivation_path);
+                    let persister = factory.clone().build(store_key);
+                    Account::new_with_xpub(
+                        xpub,
+                        script_type,
+                        self.network,
+                        derivation_path.clone(),
+                        WalletStorage(persister),
+                    )
+                    .expect("Account should be valid here")
+                } else {
+                    panic!("No mprv or xpub found. This should not happen.");
+                };
 
                 let exists = AccountSyncer::new(client.clone(), Arc::new(account))
                     .check_account_existence(discovery_address_stop_gap)
@@ -231,7 +295,10 @@ impl Wallet {
 
     pub fn get_fingerprint(&self) -> String {
         let secp = Secp256k1::new();
-        self.mprv.fingerprint(&secp).to_string()
+        match &self.key {
+            WalletMasterKey::Xpriv(mprv) => mprv.fingerprint(&secp).to_string(),
+            WalletMasterKey::Xpub(xpub) => xpub.fingerprint().to_string(),
+        }
     }
 
     pub async fn clear_store(&self) -> Result<(), Error> {
@@ -239,5 +306,99 @@ impl Wallet {
             a.clear_store().await?;
         }
         Ok(())
+    }
+
+    pub fn get_wallet_type(&self) -> WalletType {
+        match self.key {
+            WalletMasterKey::Xpub(_) => WalletType::WatchOnly,
+            WalletMasterKey::Xpriv(_) => WalletType::OnChain,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use andromeda_common::ScriptType;
+    use bitcoin::bip32::DerivationPath;
+
+    use crate::{storage::WalletMemoryPersisterFactory, wallet::WalletType};
+
+    const TEST_MNEMONIC: &str = "category law logic swear involve banner pink room diesel fragile sunset remove whale lounge captain code hobby lesson material current moment funny vast fade";
+    const XPUB: &str = "xpub6DBNnY1ewRmP4HCmfAfxod8k8V4dNXN6FcnkorJa26Pu6BpEiJh15aKGFpSLayyBXZUzw9ERcFf5z7kib4ekGW5DvGuLoE5ZExxxQqppYCf";
+    const DERIVATION_PATH: &str = "m/84'/0'/0'";
+
+    #[tokio::test]
+    async fn test_create_wallet_with_xpriv() {
+        let mut wallet = super::Wallet::new(super::Network::Bitcoin, TEST_MNEMONIC.to_string(), None).unwrap();
+
+        assert_eq!(wallet.get_network(), super::Network::Bitcoin);
+        assert_eq!(wallet.get_fingerprint(), "a7fd5114");
+        assert!(wallet.xpub().0.is_none());
+        assert!(wallet.mprv().0.is_some());
+        assert_eq!(
+            wallet.mprv().0.unwrap().to_string(),
+            "xprv9s21ZrQH143K3mp3258jgzbiGdAnGR3t3acGhzwPf9fTGq5oF5NR64f2QxJfeZDetwrvYCVQ7FwT9ZUQujso451moCWGPfRtv1m1a39V55L"
+        );
+        assert_eq!(wallet.get_wallet_type(), WalletType::OnChain);
+
+        let account = wallet.add_account(
+            ScriptType::NativeSegwit,
+            DerivationPath::from_str(DERIVATION_PATH).unwrap(),
+            WalletMemoryPersisterFactory,
+        );
+        assert!(account.is_ok());
+
+        let account = account.unwrap();
+        assert_eq!(&account.get_xpub().await.unwrap().to_string(), XPUB);
+
+        assert_eq!(
+            account.get_next_receive_address().await.unwrap().address.to_string(),
+            "bc1q8lakdutt2atnthue4j8fngj7kdqdfh6jav29ke"
+        );
+        assert_eq!(
+            account.get_next_receive_address().await.unwrap().address.to_string(),
+            "bc1qm3qzhvwfj3lycdxjyrd9ll8u5v0m8yd29qjgy7"
+        );
+        assert_eq!(
+            account.get_next_receive_address().await.unwrap().address.to_string(),
+            "bc1q28cw58duzas8k522xpjqqj5xcmpfazr0ft5juk"
+        );
+    }
+
+    #[tokio::test]
+    async fn test_create_wallet_with_xpub() {
+        let mut wallet = super::Wallet::new_watch_only(super::Network::Bitcoin, XPUB.to_owned()).unwrap();
+
+        assert_eq!(wallet.get_network(), super::Network::Bitcoin);
+        assert_eq!(wallet.get_fingerprint(), "e0cb8774");
+        assert!(wallet.xpub().0.is_some());
+        assert!(wallet.mprv().0.is_none());
+        assert_eq!(wallet.xpub().0.unwrap().to_string(), XPUB);
+        assert_eq!(wallet.get_wallet_type(), WalletType::WatchOnly);
+
+        let account = wallet.add_account(
+            ScriptType::NativeSegwit,
+            DerivationPath::from_str(DERIVATION_PATH).unwrap(),
+            WalletMemoryPersisterFactory,
+        );
+        assert!(account.is_ok());
+
+        let account = account.unwrap();
+        assert_eq!(&account.get_xpub().await.unwrap().to_string(), XPUB);
+
+        assert_eq!(
+            account.get_next_receive_address().await.unwrap().address.to_string(),
+            "bc1q8lakdutt2atnthue4j8fngj7kdqdfh6jav29ke"
+        );
+        assert_eq!(
+            account.get_next_receive_address().await.unwrap().address.to_string(),
+            "bc1qm3qzhvwfj3lycdxjyrd9ll8u5v0m8yd29qjgy7"
+        );
+        assert_eq!(
+            account.get_next_receive_address().await.unwrap().address.to_string(),
+            "bc1q28cw58duzas8k522xpjqqj5xcmpfazr0ft5juk"
+        );
     }
 }
